e9d4da9f4267d042439b486aa4a0674d7478095f
joo2296 <joo2296@gmail.com>
2024-07-17 16:49:27 +0900

    Sample1 인원추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-function Login(props){
-  return(
-    <div id = "main">
-      <h1>{props.title}</h1>
-      <input type='text' placeholder='아이디'/><br/>
-      <input type='password' placeholder='비밀번호'/><br/>
-      <input type='button' value='Login'></input>
-      <p>Don't have an account? <a href='#'>Register</a></p>
-    </div>
-  );
-}
+import { ColoredMessage } from "./components/ColoredMessage";
+import { ColoredMessage2 } from "./components/ColoredMessage2";
 
-function App() {
+import {useState} from 'react';
 
+export function App(){
+  let aaaa = useState(0);
+  console.log(aaaa);
+
+  const[num, setNum] = useState(0);
+  const divStyle = {
+    border: '1px solid blue'
+  }
+  const aaa = ()=>{
+    // alert('강아지');
+    let num2 = num+1;
+    setNum(num2);
+    alert(num2);
+  }
+  const redStyle ={
+    color:'red',
+    backgroundColor:'yellow'
+  }
+  const pinkStyle ={
+    color:'pink',
+    fontSize:'20px'
+  }
   return (
-    <div className="App">
-      <Login title="Login"></Login>
+    <div style={divStyle}>
+      {console.log('test')}
+      <p style={{color:'blue',fontSize:'32px'}}>안녕하세요</p>
+      {/* <p style={redStyle}>반갑습니다.잘지내시죠?</p> */}
+      <ColoredMessage color='orange' message='잘지내시죠?' />
+      <ColoredMessage color='red' message='How do you do?'/>
+      <ColoredMessage2 color='blue'>잘지내나요?</ColoredMessage2>
+      <p style={pinkStyle}>잘지내고있습니다.</p>
+
+      <button onClick={aaa}>버튼</button>
     </div>
   );
 }
-
-export default App;
+{/* <script>
+  function aaa(param){
+    alert(param);
+  }
+</script>
+<div >
+  <p>안녕하세요</p>
+  <p>반갑습니다.</p>
+  <button onclick="javascript:aaa('강아지')">버튼</button>
+</div> */}
--- src/components/Sample1.jsx
+++ src/components/Sample1.jsx
@@ -1,27 +1,56 @@
 import './Sample1.css'
+import React, { useState } from 'react';
 export function Sample1() {
+    // React에서의 상태 정의
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [job, setJob] = useState('');
+
+    // 사용자 추가 이벤트 핸들러
+    const handleAddUser = (e) => {
+        e.preventDefault();
+        // 새로운 사용자 객체 생성
+        const newUser = {
+            name: name,
+            age: age,
+            job: job
+        };
+        // 사용자 목록에 추가
+        setUsers([...users, newUser]);
+        // 입력 필드 초기화
+        setName('');
+        setAge('');
+        setJob('');
+    };
+
+    // 사용자 삭제 이벤트 핸들러
+    const handleDeleteUser = (index) => {
+        // 인덱스를 이용하여 사용자 목록에서 삭제
+        const updatedUsers = [...users];
+        updatedUsers.splice(index, 1);
+        setUsers(updatedUsers);
+    };
     return (
         <div>
-            <form action="" class="a">
-                <input type="button" value="추가"/>
+            <form onSubmit={handleAddUser} className="a">
+                <input type="submit" value="추가" />
                 <label htmlFor="name">이름 : </label>
-                <input type="text" placeholder="이름" id="name"/><br/>
+                <input type="text" placeholder="이름" id="name" value={name} onChange={(e) => setName(e.target.value)} /><br />
                 <label htmlFor="age">나이 : </label>
-                <input type="text" placeholder="나이" id="age"/><br/>
+                <input type="text" placeholder="나이" id="age" value={age} onChange={(e) => setAge(e.target.value)} /><br />
                 <label htmlFor="job">직업 : </label>
-                <input type="text" placeholder="직업" id="job"/>
+                <input type="text" placeholder="직업" id="job" value={job} onChange={(e) => setJob(e.target.value)} />
             </form>
             <div class="b">
-                <form action="">
-                    <div>
-                        <label htmlFor="">홍길동 (34세) 무직</label>
-                        <input type="button" value="삭제"/>
-                    </div>
-                    <div>
-                        <label htmlFor="">이순신 (54세) 군인</label>
-                        <input type="button" value="삭제"/>
-                    </div>
-                </form>
+                <ul id="userList">
+                    {users.map((user, index) => (
+                        <li key={index}>
+                            {user.name} ({user.age}세) {user.job}
+                            <button onClick={() => handleDeleteUser(index)}>삭제</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
